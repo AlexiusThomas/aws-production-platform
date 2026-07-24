b9fb38843456f00e804435ff6b946ab303e0388e
AlexiusVictoria <lexyvt@gmail.com>
2026-07-24 17:10:02 -0400

    Add reusable VPC Terraform module and deploy initial network

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_cidr     = "10.0.0.0/16"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,19 @@
 output "aws_account_id" {
-  description = "AWS account currently authenticated with Terraform."
+  description = "AWS Account ID."
   value       = data.aws_caller_identity.current.account_id
 }
 
 output "aws_region" {
-  description = "AWS region currently configured for Terraform."
+  description = "AWS Region."
   value       = data.aws_region.current.region
+}
+
+output "vpc_id" {
+  description = "ID of the application VPC."
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the application VPC."
+  value       = module.vpc.vpc_cidr_block
 }
